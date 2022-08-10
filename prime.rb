9db9dcc9fd6c7c6f2b0bdcae638e962e5af7d923
Isabella <isabella.y.hu@gmail.com>
2022-08-10 21:34:53 +1000

    commit tree 29340ec62ad1175239eb6b92e5d8c8c0f163e1e7

--- prime.rb
+++ prime.rb
@@ -1,11 +1,6 @@
 # Add  code here!
 def prime?(n)
-    a = []
-    x = 2
-    while x < n
-        a << x
-        x += 1
-    end
+    return n > 1 if n <= 3
     a = 2..(n-1)
-    n <= 1 ? false : a.none? {|x| n % x == 0}
+    a.none? {|x| n % x == 0}
 end
